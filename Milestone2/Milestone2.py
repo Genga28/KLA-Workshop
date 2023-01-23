14a656ca093afe77aa5c1e2cf0e497797d6b135f
Genga28 <82211151+Genga28@users.noreply.github.com>
2023-01-23 14:32:19 +0530

    Add files via upload

--- Milestone2/Milestone2.py
+++ Milestone2/Milestone2.py
@@ -19,19 +19,22 @@ for i in range(len(res)):
 lstind=[]
 for i in range(len(x)):
     lstind.append((x[i],y[i]))
-print(lstind)
+count2=len(lstind)
 
-file4=open('text.txt', "w")
+file4=open('Milestone2.txt', 'w')
 file3=open('Source.txt')
+boundary=''
+layer=''
+datatype=''
+xy=''
+
 for line in file3:
-    boundary=''
-    layer=''
-    datatype=''
-    xy=''
     lst1=''
     lstspare1=[]
     x1,y1=[],[]
     lstind1=[]
+    res=[]
+    res1=[]
     if line.startswith("boundary"):
         boundary=line
     elif line.startswith("layer"):
@@ -39,7 +42,7 @@ for line in file3:
     elif line.startswith("datatype"):
         datatype=line
     elif line.startswith("xy"):
-        xy=''
+        xy=line
         lst1=line.split(' ')
         while("" in lst1):
             lst1.remove("") 
@@ -50,13 +53,23 @@ for line in file3:
             if i%2==0:
                 x1.append(res1[i])
             else:
-                y1.append(res1[i]) 
-        for i in range(len(x1)):
-            lstind1.append((x1[i],y1[i]))
-        for i in lstind:
-            for j in lstind1:
-                if i==j:
-                    file4.writelines(boundary)
-                    file4.writelines(layer)
-                    file4.writelines(datatype)
-                    file4.writelines(xy)
+                y1.append(res1[i])
+        if len(x1)==len(x) and len(y1)==len(y):
+            for i in range(len(x)):
+                xo=x[i]
+                xt=x1[i]
+                yo=y[i]
+                yt=y1[i]
+                p=round(((((xt - xo)*2) + ((yt-yo)**2) )*0.5))
+                res.append(p)
+        res1 = res
+        if res==res1:
+            file4.writelines(boundary)
+            file4.writelines(layer)
+            file4.writelines(datatype)
+            file4.writelines(xy)
+            file4.writelines("endel\n")
+        del boundary
+        del layer
+        del datatype
+        del xy
